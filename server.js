97ac30f801f391ffdbe46c549e8de8024834e47e
vutomisombhane <vutomisombhane@outlook.com>
2017-02-18 03:06:16 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+﻿var http = require('http');
+var port = process.env.port || 1337;
+var express = require('express');
+var app = express();
+
+app.get('/crush', function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('My crush is Zendaya Coleman');
+});
+app.listen(port);
