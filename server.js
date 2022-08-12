69422c91c5adea2879a0e8d4d962a9c8572bd4e4
vutomisombhane <vutomisombhane@outlook.com>
2022-08-12 09:38:50 +0200

    added new test endpoints

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ var port = process.env.port || 1337;
 var express = require('express');
 var app = express();
 
-app.get('/crush', function (req, res) {
+app.get('/', function(req, res){
+    res.writeHead(200, {"Content-Type": 'text/plain '});
+    res.end("This is my default endpoint");
+});
+
+app.get('/hello', function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World');
+    res.end('Hello GitHub. This is my repository.');
 });
 app.listen(port);
